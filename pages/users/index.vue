273d06553c1aafedb56f248492c4106b36866a77
MuhammadKhizar7 <muhammadkhizar7@gmail.com>
2022-11-14 01:16:52 +0500

    feat: add user view

---
+++ pages/users/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Users </h1>
+        <div class="users">
+            <div v-for="user in users" :key="user.id">
+                <router-link :to="'/users/' + user.id">{{ user.name }}</router-link>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const { data: users } = await useFetch('/api/users')
+console.log(users);
+</script>
